7dcf7df5841735a3d0594daa43068c2ba98c1f78
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-03-04 00:08:48 +0530

    login api and dto created

--- Employee-Leave-Management-System-Bakend/src/main/java/com/example/Employee/Leave/Management/System/controller/UserController.java
+++ Employee-Leave-Management-System-Bakend/src/main/java/com/example/Employee/Leave/Management/System/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.example.Employee.Leave.Management.System.controller;
 
+import com.example.Employee.Leave.Management.System.dto.LoginRequest;
 import com.example.Employee.Leave.Management.System.entity.User;
 import com.example.Employee.Leave.Management.System.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,5 +20,20 @@ public class UserController {
     public User adduser(@RequestBody User user){
 
         return userService.registerUser(user);
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request){
+        try {
+            User user = userService.login(request.getEmail(), request.getPassword());
+            if (user != null) {
+                user.setPassword(null); // don't return password
+                return ResponseEntity.ok(user);
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+            }
+        } catch (RuntimeException ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+        }
     }
 }
---
+++ Employee-Leave-Management-System-Bakend/src/main/java/com/example/Employee/Leave/Management/System/dto/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.example.Employee.Leave.Management.System.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String email;
+    private String password;
+}
+
