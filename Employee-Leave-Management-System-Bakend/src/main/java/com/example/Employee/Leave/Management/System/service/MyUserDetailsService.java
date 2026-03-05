455c65d57fb437d8940ba7603495de37c03d0a04
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-03-05 12:23:12 +0530

    mydetaisservic addedd

---
+++ Employee-Leave-Management-System-Bakend/src/main/java/com/example/Employee/Leave/Management/System/service/MyUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.Employee.Leave.Management.System.service;
+
+import com.example.Employee.Leave.Management.System.entity.User;
+import com.example.Employee.Leave.Management.System.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
